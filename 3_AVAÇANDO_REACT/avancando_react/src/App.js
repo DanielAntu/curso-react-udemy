2f67ade0f89567e02964e33f7c491c713d854b82
DanielAntunes <antunesdaniel083@gmail.com>
2023-01-23 20:35:01 -0300

    atualização

--- 3_AVAÇANDO_REACT/avancando_react/src/App.js
+++ 3_AVAÇANDO_REACT/avancando_react/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 import City from './assets/city.jpg'
+import CarDetails from './components/CarDetails';
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const name = 'Joaquim'
+
+  const cars = [
+    {id: 1, brand: 'Ferrari', color: 'Amarelo', newCar: true, km: 0},
+    {id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 34343},
+    {id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 234}
+  ]
+
   return (
     <div className="App">
       <h1>Avançando no React</h1>
@@ -21,6 +32,18 @@ function App() {
 
       <ManageData />
       <ListRender />
+      <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={name} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+      {/*reaprovetando */}
+      <CarDetails brand="Ford" color="Vermelha" km={0} newCar={true} />
+      <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false} />
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+      ))}
     </div>
   );
 }
