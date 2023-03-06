40d32f412ebda14043dc568af7e73387281ffb52
DanielAntunes <antunesdaniel083@gmail.com>
2023-03-06 19:07:00 -0300

    atualização

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firebase"
+import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAH03rL4irOO4tlmps6Bz1ZXxKaCpPd4zE",
