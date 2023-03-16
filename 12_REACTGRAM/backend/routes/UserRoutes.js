821114814c1ef6466c15f3466aec7bf323727571
DanielAntunes <antunesdaniel083@gmail.com>
2023-03-16 19:44:05 -0300

    atualização

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -2,12 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { register } = require("../controllers/UserController");
+const {
+    register,
+    login,
+    getCurrentUser,
+    update,
+} = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
+const {
+    userCreateValidation,
+    loginValidation,
+    userUpdateValidation,
+} = require("../middlewares/userValidations");
+
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
-router.get("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+    "/",
+    authGuard,
+    userUpdateValidation(),
+    validate,
+    imageUpload.single("profileImage"),
+    update
+);
 
 module.exports = router;
