5f0a5a6e958909dc165a9fe688b836a5f2ce762f
DanielAntunes <antunesdaniel083@gmail.com>
2023-03-22 20:03:53 -0300

    atualização

--- 12_REACTGRAM/frontend/src/components/Navbar.js
+++ 12_REACTGRAM/frontend/src/components/Navbar.js
@@ -9,14 +9,28 @@ import {
 } from "react-icons/bs";
 
 // Hooks
-import { useState } from "react";
+//import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
+// Redux
+import { logout, reset } from "../slices/authSlice";
+
 const Navbar = () => {
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+
+        navigate("/login");
+    };
 
     return (
         <nav id="nav">
@@ -46,7 +60,7 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <span>Sair</span>
+                            <span onClick={handleLogout}>Sair</span>
                         </li>
                     </>
                 ) : (
