a49a95e7235bd1a38704fd389e2bab20bfe3674b
DanielAntunes <antunesdaniel083@gmail.com>
2023-03-31 17:13:04 -0300

    atualização

--- 13_TYPESCRIPT/12_todo_react/src/App.tsx
+++ 13_TYPESCRIPT/12_todo_react/src/App.tsx
@@ -1,36 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 // components
-import Header from './components/Header';
-import Footer from './components/Footer';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 // interface
-import { ITask } from './interface/Task';
+import { ITask } from "./interface/Task";
 
 function App() {
+    const [taskList, setTaskList] = useState<ITask[]>([]);
+    const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
-  const [taskList, setTaskList] = useState<ITask[]>([])
+    const deleteTask = (id: number) => {
+        setTaskList(taskList.filter((task) => task.id !== id));
+    };
 
-  return (
-    <div className="App">
-      <Header />
-      <main className={styles.main}>
-        <div>
-          <h2>O que você vai fazer?</h2>
-          <TaskForm taskList={taskList} setTaskList={setTaskList} btnText='Criar Tarefa' />
+    const hideOrShowModal = (display: boolean) => {
+        const modal = document.querySelector("#modal");
+        if (display) {
+            modal!.classList.remove("hide");
+        } else {
+            modal!.classList.add("hide");
+        }
+    };
+
+    const editTask = (task: ITask): void => {
+        hideOrShowModal(true);
+        setTaskToUpdate(task);
+    };
+
+    const updateTask = (id: number, title: string, difficulty: number) => {
+        const updatedTask: ITask = { id, title, difficulty };
+
+        const updatedItems = taskList.map((task) => {
+            return task.id === updatedTask.id ? updatedTask : task;
+        });
+
+        setTaskList(updatedItems);
+
+        hideOrShowModal(false);
+    };
+
+    return (
+        <div className="App">
+            <Modal
+                children={
+                    <TaskForm
+                        btnText="Editar tarefa"
+                        taskList={taskList}
+                        task={taskToUpdate}
+                        handleUpdate={updateTask}
+                    />
+                }
+            />
+            <Header />
+            <main className={styles.main}>
+                <div>
+                    <h2>O que você vai fazer?</h2>
+                    <TaskForm
+                        taskList={taskList}
+                        setTaskList={setTaskList}
+                        btnText="Criar Tarefa"
+                    />
+                </div>
+                <div>
+                    <h2>Suas Tarefas:</h2>
+                    <TaskList
+                        taskList={taskList}
+                        handleDelete={deleteTask}
+                        handleEdit={editTask}
+                    />
+                </div>
+            </main>
+            <Footer />
         </div>
-        <div>
-          <h2>Suas Tarefas:</h2>
-          <TaskList />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default App;
